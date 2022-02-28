a588030483c8fff6c43b39cf413605ec7129c9d5
Vishwaradhya <hvishwaradhya@gmail.com>
2022-02-28 17:02:23 +0530

    Code optimization

--- src/main/java/com/framework/actions/BaseAbstract.java
+++
@@ -1,9 +0,0 @@
-/*
- * package com.framework.actions;
- * 
- * import com.framework.initialization.DriverInitalization;
- * 
- * public abstract class BaseAbstract extends DriverInitalization {
- * 
- * }
- */
